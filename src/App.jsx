09f965f926eadd256b9390a0809a9b7a6e18ffe2
Mishael Matende <mishaelmatende@gmail.com>
2024-11-15 13:39:01 +0300

    Updated Homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
+        <Header />
         <HomePage />
       </section>
       <h1 className="text-green-400">Hello</h1>
